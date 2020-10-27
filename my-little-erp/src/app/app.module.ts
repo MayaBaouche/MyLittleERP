0eacac8ca9ff37d5887827f2eeb6da771cef8014
Charles Still <charles.still@yahoo.fr>
2020-10-27 12:32:27 +0100

    finish header/footer/welcome

--- my-little-erp/src/app/app.module.ts
+++ my-little-erp/src/app/app.module.ts
@@ -5,17 +5,25 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './formcomponent/footer/footer.component';
 import { HeaderComponent } from './formcomponent/header/header.component';
 import { from } from 'rxjs';
+import {RouterModule,Routes} from '@angular/router'
 import { WelcomeComponent } from './formcomponent/welcome/welcome.component';
 
+const routes: Routes = [
+  {
+    path:'',
+    component:WelcomeComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
     WelcomeComponent
-    ],
+      ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-little-erp/src/app/formcomponent/header/header.component.ts
+++ my-little-erp/src/app/formcomponent/header/header.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  oldSelectedId = "1";
+  isSelected1 : boolean = true;
+  isSelected2 : boolean = false;
+  isSelected3 : boolean = false;
+  isSelected4 : boolean = false;
+  isSelected5 : boolean = false;
+
+  onClickMenu( senderId){
+    if(senderId == "li1" && ("li"+this.oldSelectedId) != senderId ){
+      this.setNotSelected();
+      this.oldSelectedId = "1";
+      this.isSelected1 = true;
+    }else  if(senderId == "li2" && ("li"+this.oldSelectedId) != senderId ){
+      this.setNotSelected();
+      this.oldSelectedId = "2";
+      this.isSelected2 = true;
+    } else  if(senderId == "li3" && ("li"+this.oldSelectedId) != senderId ){
+      this.setNotSelected();
+      this.oldSelectedId = "3";
+      this.isSelected3 = true;
+    } else  if(senderId == "li4" && ("li"+this.oldSelectedId) != senderId ){
+      this.setNotSelected();
+      this.oldSelectedId = "4";
+      this.isSelected4 = true;
+    } else  if(senderId == "li5" && ("li"+this.oldSelectedId) != senderId ){
+      this.setNotSelected();
+      this.oldSelectedId = "5";
+      this.isSelected5 = true;
+    } 
+
+  }
+  setNotSelected(){
+    this.isSelected1 = false;
+    this.isSelected2 = false;
+    this.isSelected3 = false;
+    this.isSelected4 = false;
+    this.isSelected5 = false;
+  }
 }
