cc177009cbee244a13578b742173a8b5152f8ed0
Benjamin Colombo <benjamin.colombo@hotmail.com>
2020-10-27 09:30:43 +0100

    feat: implements Equipe service

--- my-little-erp/src/app/service/equipe.service.ts
+++ my-little-erp/src/app/service/equipe.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { EquipeModel, MemberModel } from '../api-model/equipe-model';
+import { environment } from '../env';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,8 @@ export class EquipeService {
 
   constructor(private httpClient: HttpClient) { }
 
-  GetEquipe(): Observable<any> {
-    return null;
+  GetEquipe(): Observable<EquipeModel> {
+    return this.httpClient.get<EquipeModel[]>
+      (environment.api.url + environment.api.routes.equipe);
   }
 }
